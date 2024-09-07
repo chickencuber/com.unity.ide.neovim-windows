be99e36f1787ec68b14eb083a41851d049b191f6
chickencuber <113270822+chickencuber@users.noreply.github.com>
2024-09-07 14:56:00 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,1 @@
-#!/bin/sh
-
-log=$HOME/.cache/nvim/unity-server.log
-
-flatpak_substring="com.unity.UnityHub"
-
-# the classic stackoverflow "how get directory of script??"
-script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-# remove the weird thing that precedes filenames
-file_arg=$(echo "$@" | sed -e "s/+normal -1G-1| //g")
-
-if [ "${FLATPAK_SANDBOX_DIR#*$flatpak_substring}" ]; then
-    # you need to run "sudo flatpak override com.unity.UnityHub --talk-name=org.freedesktop.Flatpak" for this to work
-    flatpak-spawn --host $script_dir/nvim-remote "__UNITY_FLATPAK__ $file_arg" >> $log 2>&1
-else
-    $script_dir/nvim-remote "__UNITY__ $file_arg"
-fi
+echo pick the exe
